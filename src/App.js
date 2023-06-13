b4712a5e1f713341c67ce73b1c0e40ff7d272252
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-06-13 07:20:29 +0300

    add react-fontawesome-uuidv4 to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"> First word </div>;
+  return <div className="App"> First word but updated </div>;
 }
 
 export default App;
